6bb271c4b69a394d6ffa8148403f58321e1b7c74
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-23 10:49:54 -0400

    Finished section 78

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,7 @@
 // CRUD, create, read, update, delete
 
-// const mongodb = require("mongodb");
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID;
-
 const { MongoClient, ObjectID } = require("mongodb");
 const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -21,63 +15,77 @@ MongoClient.connect(
     }
 
     const db = client.db(databaseName);
-
-    // db.collection("tasks").insertMany(
-    //   [
-    //     {
-    //       description: "Buy milk",
-    //       completed: true,
-    //     },
-    //     {
-    //       description: "Clean up desk",
-    //       completed: false,
-    //     },
-    //     {
-    //       description: "Change tires",
-    //       completed: false,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert tasks!");
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("users").insertOne(
-    //   {
-    //     _id: id,
-    //     name: "Vikram",
-    //     age: 43,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user!");
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 28,
-    //     },
-    //     {
-    //       name: "Gunther",
-    //       age: 27,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents!");
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
   }
 );
+
+// db.collection("tasks").findOne(
+//   { _id: new ObjectID("5ec818611c2f6a26de0da783") },
+//   (error, task) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     console.log(task);
+//   }
+// );
+
+// db.collection("tasks")
+//   .find({ completed: false })
+//   .toArray((error, tasks) => {
+//     console.log(tasks);
+//   });
+
+// to seach by id
+// _id: new ObjectID('id key to convert')
+// db.collection("users").findOne({ name: "Jen", age: 1 }, (error, user) => {
+//   if (error) {
+//     return console.log("Unable to fetch");
+//   }
+
+//   console.log(user);
+// });
+
+// db.collection("users")
+//   .find({ age: 27 })
+//   .toArray((error, users) => {
+//     console.log(users);
+//   });
+
+// db.collection("users")
+//   .find({ age: 27 })
+//   .count((error, count) => {
+//     console.log(count);
+//   });
+
+// db.collection("users").insertOne(
+//   {
+//     _id: id,
+//     name: "Vikram",
+//     age: 43,
+//   },
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert user!");
+//     }
+
+//     console.log(result.ops);
+//   }
+// );
+
+// db.collection("users").insertMany(
+//   [
+//     {
+//       name: "Jen",
+//       age: 28,
+//     },
+//     {
+//       name: "Gunther",
+//       age: 27,
+//     },
+//   ],
+//   (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert documents!");
+//     }
+//     console.log(result.ops);
+//   }
+// );
