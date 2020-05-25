8d04f4371312270c61380d7385b2f91e18fe01b3
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-25 12:24:51 -0400

    Finished section 86

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const url = "mongodb://127.0.0.1:27017/task-manager-api";
 
 mongoose.connect(url, {
@@ -7,16 +8,51 @@ mongoose.connect(url, {
   useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid.");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot be "password"');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number.");
+      }
+    },
+  },
+});
+
+// const me = new User({
+//   name: "   Andrew  ",
+//   email: "MEAD@MEAD.IO    ",
+//   password: "  passw0rd  ",
 // });
 
-// const me = new User({ name: "Jeremy", age: 30 });
 // me.save()
 //   .then(() => {
 //     console.log(me);
@@ -26,11 +62,14 @@ mongoose.connect(url, {
 //   });
 
 const Task = mongoose.model("Task", {
-  description: { type: String },
-  completed: { type: Boolean },
+  description: { type: String, trim: true, required: true },
+  completed: { type: Boolean, required: false, default: false },
 });
 
-const t = new Task({ description: "Buy some milk", completed: false });
+const t = new Task({
+  description: " make some noooooiiiiiisssseeeeee   ",
+  completed: false,
+});
 t.save()
   .then(() => {
     console.log(t);
