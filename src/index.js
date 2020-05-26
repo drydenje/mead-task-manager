c33fa7d47d69343c6149feb04b5e5e0cfff140af
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-26 11:58:38 -0400

    Finished section 92

--- src/index.js
+++ src/index.js
@@ -59,3 +59,27 @@ app.post("/tasks", (req, res) => {
       res.status(400).send(error);
     });
 });
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
