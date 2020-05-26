495505896bae6ba33932e85818ef131e5cf49247
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-26 19:22:26 -0400

    Finished section 99

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const Task = require("./models/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log("Server is up on port:", port);
-});
-
 app.use(express.json());
 
 app.post("/users", async (req, res) => {
@@ -107,4 +103,38 @@ app.get("/tasks/:id", async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!!" });
+  }
+
+  const _id = req.params.id;
+  const _body = req.body;
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, _body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is up on port:", port);
 });
