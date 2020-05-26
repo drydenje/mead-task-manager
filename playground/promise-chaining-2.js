1d06b6aa2dc7b8376821a11a8bb3535766befdf6
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-26 14:49:58 -0400

    Finished section 96

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,12 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-const _id = "5ecbf08e63e34f277c0e047d";
-Task.findByIdAndDelete(_id)
-  .then((task) => {
-    return Task.countDocuments({ completed: false });
+// const _id = "5ecbf08e63e34f277c0e047d";
+// Task.findByIdAndDelete(_id)
+//   .then((task) => {
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((results) => {
+//     console.log("Count:", results);
+//   })
+//   .catch((e) => console.log(e));
+
+const _id = "5ecbf06afff37627402c1405";
+const deleteTaskAndCount = async (id) => {
+  const user = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount(_id)
+  .then((count) => {
+    console.log("Count:", count);
   })
-  .then((results) => {
-    console.log("Count:", results);
-  })
-  .catch((e) => console(e));
+  .catch((e) => {
+    console.log(e);
+  });
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("5ecc6c80bd1c2b65d2893823", { age: 1 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then((result) => {
-    console.log(result);
+// User.findByIdAndUpdate("5ecc6c80bd1c2b65d2893823", { age: 1 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("5ecc6c80bd1c2b65d2893823", 2)
+  .then((count) => {
+    console.log("Count:", count);
   })
   .catch((e) => {
     console.log(e);
