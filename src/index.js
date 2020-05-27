8cfe8cc192810b45fddda88c82d0dcd6e3547e99
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-27 13:23:34 -0400

    Finished section 103

--- src/index.js
+++ src/index.js
@@ -13,3 +13,18 @@ app.use(routers);
 app.listen(port, () => {
   console.log("Server is up on port:", port);
 });
+
+const bcrypt = require("bcryptjs");
+
+const myFunction = async () => {
+  const password = "Red12345!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log("Password:", password);
+  console.log("Hashed:", hashedPassword);
+
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
