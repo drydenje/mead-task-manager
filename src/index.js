1437ddd2bd4cd4238bf1855ce333734bca68e151
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-06-05 15:54:51 -0400

    Finished section 114

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ app.use(routers);
 app.listen(port, () => {
   console.log("Server is up on port:", port);
 });
+
+const Task = require("./models/task");
+const User = require("./models/user");
+const main = async () => {
+  // const task = await Task.findById("5eda9dd575e1530dfbb7e072");
+  // await task.populate("owner").execPopulate();
+  // console.log(task.owner);
+  const user = await User.findById("5eda9bed235a1b0b9d859227");
+  await user.populate("tasks").execPopulate();
+
+  console.log(user.tasks);
+};
+
+main();
