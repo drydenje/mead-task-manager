0d261e3ae77aa230d7c16045fe9cc7689ecb4693
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-06-09 19:21:08 -0400

    Finished section 124

--- src/index.js
+++ src/index.js
@@ -12,6 +12,21 @@ const port = process.env.PORT || 3000;
 const multer = require("multer");
 const upload = multer({
   dest: "images",
+  limits: {
+    // in bytes (1mb = 1000000)
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload a Word document"));
+    }
+
+    cb(undefined, true);
+
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cd(undefined, false)
+  },
 });
 app.post("/upload", upload.single("upload"), (req, res) => {
   res.send();
